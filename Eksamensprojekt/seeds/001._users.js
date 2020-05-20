f5e1897f6d4c2ec32efda1ddd0af7806d9da7b06
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-20 05:15:39 +0200

    added extra users

--- Eksamensprojekt/seeds/001._users.js
+++ Eksamensprojekt/seeds/001._users.js
@@ -2,7 +2,9 @@
 exports.seed = function(knex) {
        // Inserts seed entries
       return knex('users').insert([
-        {username: "Sof", password: "secret123"},
-        {username: "And", password: "dobblesecret"},
+        {username: "Sof", password: "secret123", email: "sof@fam1.dk"},
+        {username: "And", password: "dobblesecret", email: "and@fam1.dk"},
+        {username: "Nad", password: "password", email: "nad@fam2.dk"},
+        {username: "Dan", password: "password", email: "dan@fam2.dk"},
       ]);
 };
