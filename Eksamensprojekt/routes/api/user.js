8b496f20f0c73c1f1e78daa88e5d8cb78b51320b
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-23 19:22:05 +0200

    moved signup endpoint from /api/auth/signup to /api/user/

--- Eksamensprojekt/routes/api/user.js
+++ Eksamensprojekt/routes/api/user.js
@@ -10,55 +10,70 @@ const saltRounds = 12;
 
 router.post("/api/user", async (req, res) => {
 
-    if (!(req.session.userId && req.session.householdId)) {
-        return res.status(401).send({ response: "Unauthorized access" });
+    const { username: username, password, email, household } = req.body;
+
+    // missing variables validation
+    if (!(username && password && email && household))
+    {
+        return res.status(400).send({ response: "username, password, email or household missing" });
     }
 
-    const householdId = req.session.householdId;
-    const { username, password, email } = req.body;
+    // password validation
+    if (password.length < 8) {
+        return res.status(400).send({ response: "password must be 8 characters or longer" });
+    }
 
-    // username or password missing
-    if (!(username && password && email)) {
-        return res.status(400).send({ response: "username, password or email missing" });
+    // email validation
+    if (!validateEmail(email)) {
+        return res.status(400).send({ response: "email syntax is wrong" });
     }
 
     try {
+        const foundUser = await User.query().where({ username }).orWhere({ email }).first();
+        if (foundUser) {
+            return res.status(400).send({ response: "username or email already exist in the system" });
+        }
+
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        try {// create user
-            const household = await Household.query().findById(householdId);
-
-            if (household.ownerId != req.session.userId) {
-                return res.status(401).send({ response: `Unauthorized access: authenticated user is not owner of household ${ household.name }`});
-            }
-
-            const foundUser = await User.query().where({ username }).orWhere({ email }).first();
-            if (foundUser) {
-                return res.status(400).send({ response: "username or email already exist in the system" });
-            }
-
-            // insert user to db
-            const newUser = await household.$relatedQuery('members').insertAndFetch(
+        // Start new transaction.
+        // All queries in this transaction will be rolled back upon exception
+        const newUser = await User.transaction(async trx => {
+            // We query household on the transaction
+            const newHousehold = await Household.query(trx).insertGraphAndFetch(
             {
-                username,
-                password: hashedPassword,
-                email,
-                household: household
-            });
+                name: household,
+                memberCount: 1,
+                owner: {
+                    "#id": "householdOwner",
+                    username,
+                    password: hashedPassword,
+                    email,
+                },
+                members: [ { "#ref": "householdOwner" } ],
+            },
+            { allowRefs: true });
+
+            // fetch newly created user with graph( household )
+            const newUser = await User.query(trx).first().withGraphFetched("household").where({id: newHousehold.owner.id});
+            
+            /*
+            const newUser = newHousehold.owner;
+            newUser.household = newHousehold;
+            */
+
             delete newUser.password;
-    
-            return res.status(201).send({response: newUser});
-    
-        } catch(err) {
-            // this error should be logged
-            // should probably not send err to client.
-            return res.send({response: "Error with DB" + err});
-        }
-    } catch(err) {
-        // this error should be logged
-        // should probably not send err to client.
-        return res.send({response: "Error encrypting password" + err});
-    }
+      
+          // Whatever we return from the transaction callback gets returned
+          // from the `transaction` function.
+          return newUser;
+        });
+        // Here the transaction has been committed.
+        return res.status(201).send({ response: newUser });
+      } catch (err) {
+        // Here the transaction has been rolled back.
+        return res.status(500).send({ response: "Something went wrong with the DB" + err });
+      }
 });
 
 
