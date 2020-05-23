2c752874b76ac4c81e54cf53c185586ddc3abc21
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-23 19:20:25 +0200

    HouseholdMembersList_populate() will now fetch household w. members and populate members list

--- Eksamensprojekt/public/household/household.js
+++ Eksamensprojekt/public/household/household.js
@@ -1,19 +1,47 @@
-async function fetchHousehold() {
-    const response = await fetch("/api/househould");
+async function HouseholdMembersList_populate() {
+    const response = await fetch("/api/household");
+
     if (response.status != 200) {
         // err
         return;
     }
 
     const household = (await response.json()).response;
+    $(".household-view .household-name").text(household.name);
+
+    const householdMemberListElement = $(".household-memberslist");
+    const householdMemberDOMTemplate = $(".household-memberslist .row")[0];
+
+    console.log(household.members);
 
     // populate household-memberslist with DOM-elements
+    household.members.forEach(member => {
+        const householdMemberDOMElement = $(householdMemberDOMTemplate).clone();
+        
+        const bntRemoveMember = householdMemberDOMElement.find(".household-member-btn-remove");
 
+        $(householdMemberDOMElement).find(".household-member-name").text(member.username);
+        bntRemoveMember.attr("alt", (bntRemoveMember.attr("alt") + member.username));
+        bntRemoveMember.attr("data-username", member.username );
+        bntRemoveMember.attr("data-userId", member.id );
+
+        householdMemberListElement.append(householdMemberDOMElement);
+    });
 }
 
 
-function householdMemberRemove() {
-    return Exception("401 - Not yet implemented");
+function householdMemberRemove(src)
+{
+    const username = $(src).attr("data-username");
+
+    if (!confirm(`Are you sure?\nThis will completely remove the member ${username} from the system.`))
+    {
+        return false;
+    }
+
     // TODO:
     // fetch api route (del: /api/household/members/{id})
+
+
+    return true;
 }
