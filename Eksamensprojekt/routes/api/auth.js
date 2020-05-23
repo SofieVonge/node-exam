c57731a134264b436312a9f40438963dc6ba4ee3
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-23 19:21:53 +0200

    moved signup endpoint from /api/auth/signup to /api/user/

--- Eksamensprojekt/routes/api/auth.js
+++ Eksamensprojekt/routes/api/auth.js
@@ -7,74 +7,6 @@ const User = require("../../models/User.js");
 const Household = require("../../models/Household.js");
 
 
-router.post("/api/auth/signup", async (req, res) =>
-{
-    const { username: username, password, email, household } = req.body;
-
-    // missing variables validation
-    if (!(username && password && email && household))
-    {
-        return res.status(400).send({ response: "username, password, email or household missing" });
-    }
-
-    // password validation
-    if (password.length < 8) {
-        return res.status(400).send({ response: "password must be 8 characters or longer" });
-    }
-
-    // email validation
-    if (!validateEmail(email)) {
-        return res.status(400).send({ response: "email syntax is wrong" });
-    }
-
-    try {
-        const foundUser = await User.query().where({ username }).orWhere({ email }).first();
-        if (foundUser) {
-            return res.status(400).send({ response: "username or email already exist in the system" });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-        // Start new transaction.
-        // All queries in this transaction will be rolled back upon exception
-        const newUser = await User.transaction(async trx => {
-            // We query household on the transaction
-            const newHousehold = await Household.query(trx).insertGraphAndFetch(
-            {
-                name: household,
-                memberCount: 1,
-                owner: {
-                    "#id": "householdOwner",
-                    username,
-                    password: hashedPassword,
-                    email,
-                },
-                members: [ { "#ref": "householdOwner" } ],
-            },
-            { allowRefs: true });
-
-            // fetch newly created user with graph( household )
-            const newUser = await User.query(trx).first().withGraphFetched("household").where({id: newHousehold.owner.id});
-            
-            /*
-            const newUser = newHousehold.owner;
-            newUser.household = newHousehold;
-            */
-
-            delete newUser.password;
-      
-          // Whatever we return from the transaction callback gets returned
-          // from the `transaction` function.
-          return newUser;
-        });
-        // Here the transaction has been committed.
-        return res.status(201).send({ response: newUser });
-      } catch (err) {
-        // Here the transaction has been rolled back.
-        return res.status(500).send({ response: "Something went wrong with the DB" + err });
-      }
-});
-
 router.post("/api/auth/signin", async (req, res) => {
     const { username, password } = req.body;
 
@@ -100,6 +32,21 @@ router.post("/api/auth/signin", async (req, res) => {
         }
     } catch (ex) {
         return res.status(500).send({ response: "something went wrong with the DB" })
+    }
+});
+
+router.post("/api/auth/signout", async (req, res) => {
+    
+
+    try
+    {
+        delete req.session.userId;
+        delete req.session.householdId;
+
+        return res.status(200).send({ response: true });
+    } catch (err) {
+        // this err should be logged to database.
+        return res.status(500).send({ response: `something went wrong. You were not signed out correctly.\n\r Error: ${ err }` })
     }
 });
 
