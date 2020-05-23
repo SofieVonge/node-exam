75f8fcc8faa44525cda6e92479793ed197a3dad4
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-23 14:29:05 +0200

    created /api/user but not implemented

--- Eksamensprojekt/routes/api/user.js
+++ Eksamensprojekt/routes/api/user.js
@@ -3,5 +3,9 @@ const router = require("express").Router();
 const User = require("../../models/User.js");
 
 
+router.post("/api/user", (req, res) => {
+    return res.status(501).send({ response: "Not yet implemented" });
+});
+
 
 module.exports = router;
