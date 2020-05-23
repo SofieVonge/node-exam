92b1be64c39182694dfe99233fd60be9144a56f6
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-23 14:29:22 +0200

    Create household.js

---
+++ Eksamensprojekt/routes/controller/household.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const fs = require("fs");
+
+const navbarView = fs.readFileSync("./public/navbar/navbar.html", "utf8");
+const footerView = fs.readFileSync("./public/footer/footer.html", "utf8");
+
+const newMemberView = fs.readFileSync("./public/household/createmember/createmember.html", "utf8");
+
+
+router.get("/household/members/new", (req, res) => {
+    return res.send(navbarView + expenseView + footerView);
+});
+
+module.exports = router;
