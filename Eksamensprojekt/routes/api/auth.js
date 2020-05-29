14636d96bb8aad73aa5c3740118ae829b1e055d6
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-29 11:48:34 +0200

    removed unnecessary function validateEmail

--- Eksamensprojekt/routes/api/auth.js
+++ Eksamensprojekt/routes/api/auth.js
@@ -50,11 +50,4 @@ router.post("/api/auth/signout", async (req, res) =>
 });
 
 
-
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-
 module.exports = router;
