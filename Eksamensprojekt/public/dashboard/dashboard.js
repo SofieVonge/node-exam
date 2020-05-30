d487df30b35fd14999732411434febb1798344a7
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-05-30 19:28:00 +0200

    fixed paymentDate format

--- Eksamensprojekt/public/dashboard/dashboard.js
+++ Eksamensprojekt/public/dashboard/dashboard.js
@@ -2,10 +2,11 @@ fetch("/api/summaries")
         .then(response => response.json())
         .then(data => {
             data.response.map((summary) => {
+                console.log(summary.paymentDate);
                 const paymentDate = new Date(summary.paymentDate);
                 let summaryElement =  $(".summary").first().clone();
                 $(summaryElement).find("#total").html(summary.total / summary.household.memberCount);
-                $(summaryElement).find("#date").html(paymentDate.getDay() + "/" + paymentDate.getMonth() + "/" + paymentDate.getFullYear());
+                $(summaryElement).find("#date").html(paymentDate.getDate() + "/" + (paymentDate.getUTCMonth() + 1) + "/" + paymentDate.getFullYear());
                 $(summaryElement).find("#link").html(`<a href="/summaries/detail/${summary.id}" class="btn btn-info" role="button">Show details</a>`);
                 $(".summary-container").append(summaryElement);
             });     
