d85f56c021da306e4effe308a1735a8e21504695
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-06-01 12:40:03 +0200

    minor refactoring

--- Eksamensprojekt/routes/service/summaryMaker.js
+++ Eksamensprojekt/routes/service/summaryMaker.js
@@ -5,7 +5,6 @@ const Summary = require("../../models/Summary.js");
 const Expense = require("../../models/Expense.js");
 
 
-
 function ensureTwoDigitString(num) {
     if (0 <= num && num < 10) {
         return `0${num}`;
@@ -35,8 +34,6 @@ async function fetchHouseholdsWithMissingSummary(month, year) {
     const createdAtFrom = new Date(year, month, 1); // start of month
     const createdAtTo = new Date(year, month + 1, 0);// end of month
 
-    console.log("M:", month, createdAtFrom.toMysqlFormat(), createdAtTo.toMysqlFormat());
-
     const summaryQuery = Household.relatedQuery("summaries").whereBetween("summaries.paymentDate", [createdAtFrom.toMysqlFormat(), createdAtTo.toMysqlFormat()]);
     const households = await Household.query().whereNotExists(summaryQuery);
 
@@ -44,66 +41,16 @@ async function fetchHouseholdsWithMissingSummary(month, year) {
 }
 
 
-function setNextPayment(expense) {
-    
-    let next = expense.nextPayment + expense.timeBetween;
-    expense.nextPayment = next > 12 ? next - 12 : next;
-    //const id = expense.id;
-/*
-    switch (expense.timeBetween) {
-
-        case 1:
-            if (next < 12) {
-                updateNextPayment(next++, id);
-            } else{
-                updateNextPayment(1, id);
-            }
-            return;
-        case 2:
-            next = next + 2;
-            break;
-        case 3:
-            next = next + 3;
-            break;
-        case 4:
-            next = next + 4;
-            break;
-        case 6:
-            next = next + 6;
-           break;
-        case 12:
-            //not needed to do anything here?
-           // updateNextPayment(next, id);
-            return;
-         
-    }*/
-    
-    /*if (next > 12) {
-        updateNextPayment(next - 12, id);
-    } else {
-        updateNextPayment(next, id);
-    }*/
-}
-
-async function updateNextPayment(next, id) {
-    try {
-        const numUpdated = await Expense.query().findById(id).patch({
-            nextPayment: next
-        });
-    } catch (error) {
-        console.log(error);
-    }   
-}
-
 
 async function createSummary(householdId, month, year) {
-    console.log("month:", (month + 1));
     const expenses = await Expense.query().where({nextPayment: (month + 1), householdId});
-    console.log("exp:", expenses.length);
     let total = 0;
     expenses.map(expense => {
         total += expense.amount;
-        setNextPayment(expense);
+
+        // set next payment for expense
+        let nextPayment = expense.nextPayment + expense.timeBetween;
+        expense.nextPayment = nextPayment > 12 ? nextPayment - 12 : nextPayment;
     });
 
     // transaction will rollback if an exception is thrown
@@ -117,8 +64,6 @@ async function createSummary(householdId, month, year) {
         }
 
         const paymentDate = new Date(year, (month-1), 20);
-
-        //console.log(paymentDate);
 
         // insert summary with relations to  expenses
         const summary = await Summary.query(trx).upsertGraphAndFetch({
