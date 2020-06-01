ac2f7a9189933ff5659f756ee71120830a6bd144
Daniel Blom <42900979+dani153i@users.noreply.github.com>
2020-06-01 18:26:57 +0200

    fixed datetime conversion issues & createSummaries() is now working.

--- Eksamensprojekt/routes/service/summaryMaker.js
+++ Eksamensprojekt/routes/service/summaryMaker.js
@@ -5,15 +5,36 @@ const Summary = require("../../models/Summary.js");
 const Expense = require("../../models/Expense.js");
 
 
+
+function ensureTwoDigitString(num) {
+    if (0 <= num && num < 10) {
+        return `0${num}`;
+    }
+    if(-10 < num && num < 0) {
+        return "-0" + (-1*d).toString();
+    }
+    return num.toString();
+}
+
+/**
+ * 
+ **/
+Date.prototype.toMysqlFormat = function() {
+    return this.getFullYear() + "-" + ensureTwoDigitString(this.getMonth()) + "-" + ensureTwoDigitString(this.getDate()) + "T" + ensureTwoDigitString(this.getHours()) + ":" + ensureTwoDigitString(this.getMinutes()) + ":" + ensureTwoDigitString(this.getSeconds()) + ".000Z";
+};
+
+
+
 router.get("/testRoute", async (req, res) => {
-    return res.send(await fetchHouseholdsWithMissingSummary(5, 2020));
+    //return res.send(await fetchHouseholdsWithMissingSummary(5, 2020));
+    return res.send(await createSummaries());
 });
 
 async function fetchHouseholdsWithMissingSummary(month, year) {
     const createdAtFrom = new Date(year, month, 1); // start of month
     const createdAtTo = new Date(year, month + 1, 0);// end of month
 
-    const summaryQuery = Household.relatedQuery("summaries").whereBetween("summaries.createdAt", [createdAtFrom, createdAtTo]);
+    const summaryQuery = Household.relatedQuery("summaries").whereBetween("summaries.paymentDate", [createdAtFrom.toMysqlFormat(), createdAtTo.toMysqlFormat()]);
     const households = await Household.query().whereNotExists(summaryQuery);
 
     return households;
@@ -23,7 +44,8 @@ async function fetchHouseholdsWithMissingSummary(month, year) {
 function setNextPayment(expense) {
     
     let next = expense.nextPayment + expense.timeBetween;
-    const id = expense.id;
+    expense.nextPayment = next > 12 ? next - 12 : next;
+    //const id = expense.id;
 /*
     switch (expense.timeBetween) {
 
@@ -53,11 +75,11 @@ function setNextPayment(expense) {
          
     }*/
     
-    if (next > 12) {
+    /*if (next > 12) {
         updateNextPayment(next - 12, id);
     } else {
         updateNextPayment(next, id);
-    }
+    }*/
 }
 
 async function updateNextPayment(next, id) {
@@ -72,11 +94,11 @@ async function updateNextPayment(next, id) {
 
 
 async function createSummary(householdId, month, year) {
-    const expenses = await Expense.query().where("nextPayment", month);
+    const expenses = await Expense.query().where({nextPayment: month + 1, householdId});
 
     let total = 0;
-    summary.expenses.map(expense => {
-        total++;
+    expenses.map(expense => {
+        total += expense.amount;
         setNextPayment(expense);
     });
 
@@ -84,14 +106,19 @@ async function createSummary(householdId, month, year) {
     const summary = await Summary.transaction(async (trx) => {
         
         const upsertOptions = {
+            insertMissing: true,
             relate: ['expenses'],
             update: ['expenses'],
             noDelete: ['expenses']
         }
 
+        const paymentDate = new Date(year, month, 20, 0, 0, 0, 1);
+
+        //console.log(paymentDate);
+
         // insert summary with relations to  expenses
         const summary = await Summary.query(trx).upsertGraphAndFetch({
-            paymentDate: new Date(year, month, 20, 0, 0, 0, 0),
+            paymentDate: paymentDate.getFullYear() + "-" + paymentDate.getMonth() + "-20",
             total,
             householdId,
             expenses,
@@ -104,15 +131,28 @@ async function createSummary(householdId, month, year) {
 }
 
 let createSummaries = async function () {
+    let summaries = [];
     const now = new Date();
     now.setMonth(now.getMonth() + 1);
     // household with no summary for next month (created this month)
+    //const households = await fetchHouseholdsWithMissingSummary(now.getMonth(), now.getFullYear());
+    
     const households = await fetchHouseholdsWithMissingSummary(now.getMonth(), now.getFullYear());
+    console.log(households);
 
-    // create summary for each household
-    for (let i in households) {
-        createSummary(households[i].id, now);
+
+
+    try {
+        // create summary for each household
+        for (let i in households) {
+            summaries.push(await createSummary(households[i].id, now.getMonth(), now.getUTCFullYear()));
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
     }
+
+    return summaries;
 }
 
 module.exports = {
